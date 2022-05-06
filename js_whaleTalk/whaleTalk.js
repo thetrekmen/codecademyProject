6e1e3cc41dd4570fbfb105cdb75a20fb19010a54
thetrekmen <jonespm1@gmail.com>
2022-05-06 04:22:58 -0400

    added whale talk project

---
+++ js_whaleTalk/whaleTalk.js
@@ -0,0 +1,31 @@
+/*Take a phrase like ‘turpentine and turtles’ and translate it into its “whale talk” equivalent: ‘UUEEIEEAUUEE’.
+
+There are a few simple rules for translating text to whale language:
+
+There are no consonants. Only vowels excluding “y”.
+The u‘s and e‘s are extra long, so we must double them in our program.*/
+
+
+let input = "they told me lifes a bitch so i fucked her raw";
+const vowels = ["a", "e", "i", "o", "u"];
+
+let resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+  // console.log(input[i] + i);
+  if (input[i] === vowels["1"]) {
+    resultArray.push(input[i]);
+  }
+ if (input[i] === vowels["4"]) {
+    resultArray.push(input[i]);
+  }
+  for (let j = 0; j < vowels.length; j++) {
+    // console.log(vowels[j] + j);
+    if (input[i] === vowels[j]) {
+      resultArray.push(input[i]);
+    } 
+  } 
+} 
+
+const resultArrayString = resultArray.join("")
+console.log(resultArrayString.toUpperCase())
